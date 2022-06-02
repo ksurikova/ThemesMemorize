613dd6f70feb8685af9b3539297008cb8671b00c
ksurikova <ksantisurikova@gmail.com>
2022-06-02 08:51:51 +0300

    Initial Commit

---
+++ ThemesMemorize/ThemesMemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThemesMemorizeApp.swift
+//  ThemesMemorize
+//
+//  Created by Ksenia Surikova on 02.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct ThemesMemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
